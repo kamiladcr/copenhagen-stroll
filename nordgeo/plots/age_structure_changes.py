61d1ccc4a5f525604e69ef1285ecac0546c9f22c
Kamila Dzhavatova <kamila.dzhavatova@gmail.com>
2023-05-21 19:10:25 +0200

    update plots

--- nordgeo/plots/age_structure_changes.py
+++ nordgeo/plots/age_structure_changes.py
@@ -1,9 +1,7 @@
 from nordgeo import data
 import plotly.graph_objects as go
-
-# The distribution of age groups' populations over time
-# Each area represents an age group, and the y-axis represents the population count.
-
+import geopandas as gpd
+import plotly.express as px
 
 def show():
     df = data.load()
@@ -37,3 +35,102 @@ def show():
 
     fig = go.Figure(data=traces, layout=layout)
     fig.show()
+
+
+def working():
+    df = data.load()
+    df = df[df["age group"] == "working age"]
+    df = df[df["year"] != "2018"]
+
+    df["population dynamics"] = (
+        (df["value_grouped"] - df["value_grouped_prev"])
+        / df["value_grouped_prev"]
+        * 100
+    )
+
+    gdf = gpd.GeoDataFrame(df)
+    gdf = gdf.drop(["centroid"], axis=1)
+
+    px.choropleth_mapbox(
+        title="Population change in 2019-2022 for working age group",
+        data_frame=gdf,
+        geojson=gdf._to_geo(),
+        featureidkey="properties.municipality",
+        locations="municipality",
+        color="population dynamics",
+        range_color=(-5, +5),
+        color_continuous_scale="RdBu",
+        mapbox_style="carto-positron",
+        zoom=6,
+        center={"lat": 56.05, "lon": 12.70},
+        opacity=0.7,
+        labels={"value": "Value"},
+        animation_frame="year",
+        height=800,
+    ).show()
+
+
+def youth():
+    df = data.load()
+    df = df[df["age group"] == "youth"]
+    df = df[df["year"] != "2018"]
+
+    df["population dynamics"] = (
+        (df["value_grouped"] - df["value_grouped_prev"])
+        / df["value_grouped_prev"]
+        * 100
+    )
+
+    gdf = gpd.GeoDataFrame(df)
+    gdf = gdf.drop(["centroid"], axis=1)
+
+    px.choropleth_mapbox(
+        title="Population change in 2019-2022 for youth age group",
+        data_frame=gdf,
+        geojson=gdf._to_geo(),
+        featureidkey="properties.municipality",
+        locations="municipality",
+        color="population dynamics",
+        range_color=(-5, +5),
+        color_continuous_scale="RdBu",
+        mapbox_style="carto-positron",
+        zoom=6,
+        center={"lat": 56.05, "lon": 12.70},
+        opacity=0.7,
+        labels={"value": "Value"},
+        animation_frame="year",
+        height=800,
+    ).show()
+
+
+def elderly():
+    df = data.load()
+    df = df[df["age group"] == "elderly"]
+    df = df[df["year"] != "2018"]
+
+    df["population dynamics"] = (
+        (df["value_grouped"] - df["value_grouped_prev"])
+        / df["value_grouped_prev"]
+        * 100
+    )
+
+    gdf = gpd.GeoDataFrame(df)
+    gdf = gdf.drop(["centroid"], axis=1)
+
+    px.choropleth_mapbox(
+        title="Population change in 2019-2022 for elderly age group",
+        data_frame=gdf,
+        geojson=gdf._to_geo(),
+        featureidkey="properties.municipality",
+        locations="municipality",
+        color="population dynamics",
+        range_color=(-5, +5),
+        color_continuous_scale="RdBu",
+        mapbox_style="carto-positron",
+        zoom=6,
+        center={"lat": 56.05, "lon": 12.70},
+        opacity=0.7,
+        labels={"value": "Value"},
+        animation_frame="year",
+        height=800,
+    ).show()
